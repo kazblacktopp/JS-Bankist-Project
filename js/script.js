46075bc514548cac026582dc31be0c9696fce9e0
kazblacktopp <karen.blacktopp@gmail.com>
2021-09-14 09:35:22 +1000

    Create function for stick navigation bar on page scroll

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-"use strict";
+'use strict';
 
 ///////////////////////////////////////////////////////////////
 // Fix for flexbox gap property missing in some Safari browsers
 
 const checkFlexGap = () => {
   // Create a flex container with gap
-  const flexEl = document.createElement("div");
-  flexEl.style.display = "flex";
-  flexEl.style.flexDirection = "column";
-  flexEl.style.gap = "1px";
+  const flexEl = document.createElement('div');
+  flexEl.style.display = 'flex';
+  flexEl.style.flexDirection = 'column';
+  flexEl.style.gap = '1px';
 
   // Append two child elements to flexEl container
-  flexEl.appendChild(document.createElement("div"));
-  flexEl.appendChild(document.createElement("div"));
+  flexEl.appendChild(document.createElement('div'));
+  flexEl.appendChild(document.createElement('div'));
 
   // Append flexEl container to DOM IOT read scrollHeight
   document.body.appendChild(flexEl);
@@ -23,9 +23,30 @@ const checkFlexGap = () => {
 };
 
 if (!checkFlexGap()) {
-  document.body.classList.add("js-no-flexbox-gap");
-  console.log("Flexbox gap is not supported");
-  console.log(document.body);
+  document.body.classList.add('js-no-flexbox-gap');
+  console.log('Flexbox gap is not supported');
 }
+
 ///////////////////////////////////////////////////////////////
-// Create Sticky Navigation Bar
+// Create Sticky Navigation Bar on page scroll
+
+const heroEl = document.querySelector('.section__hero');
+const navBarEl = document.querySelector('.nav');
+
+// Get height of nav bar and convert to negative pixels
+const navBarOffset = '-' + navBarEl.offsetHeight + 'px';
+
+const obs = new IntersectionObserver(
+  entries => {
+    const ent = entries[0];
+    if (!ent.isIntersecting) document.body.classList.add('js-sticky');
+    if (ent.isIntersecting) document.body.classList.remove('js-sticky');
+  },
+  {
+    root: null /* Viewport */,
+    threshold: 0,
+    rootMargin: navBarOffset,
+  }
+);
+
+obs.observe(heroEl);
